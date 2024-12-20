2469f8a0279fda053f0265940e06d3cc3a5c1067
alikashif369 <kashif107712@gmail.com>
2024-12-21 01:40:03 +0500

    update frontend buyerlogin

--- auth/BuyerLogin.php
+++ auth/BuyerLogin.php
@@ -1,292 +1,179 @@
 <?php
 session_start();
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Buyer login</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <a href="https://icons8.com/icon/83325/roman-soldier"></a>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <title>Buyer Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/c587fc1763.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"></script>
     <style>
-        @import url(https://fonts.googleapis.com/css?family=Raleway:300,400,600);
-
-        color: goldenrod;
-        margin-top: 15px;
-        }
-
-        #a {
-            text-align: center;
-            font-size: 25px;
-            border-style: solid;
-            /*margin-border-top: 8%;*/
-            margin-top: 10%;
-            margin-left: 28%;
-            margin-right: 20%;
-            margin-bottom: 18%;
-            max-width: 40%;
-            min-width: 20%;
-        }
-
-        .aligncenter {
-            text-align: center;
-        }
-
-        .aligncenter {
-            text-align: center;
-        }
-
-        a {
-            color: goldenrod;
-        }
-
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        nav {
-            background-color: #292b2c;
-        }
-
-        .navbar-custom {
-            background-color: #292b2c;
-        }
-
-        /* change the brand and text color */
-        .navbar-custom .navbar-brand,
-        .navbar-custom .navbar-text {
-            background-color: #292b2c;
-        }
-
-        .navbar-custom .navbar-nav .nav-link {
-            background-color: #292b2c;
-        }
-
-        .navbar-custom .nav-item.active .nav-link,
-        .navbar-custom .nav-item:hover .nav-link {
-            background-color: #292b2c;
-        }
-
-
-        .mybtn {
-            border-color: green;
-            border-style: solid;
-        }
-
-
-        .right {
-            display: flex;
-        }
-
-        .left {
-            display: none;
-        }
-
-        .cart {
-            /* margin-left:10px; */
-            margin-right: -9px;
-        }
-
-        .profile {
-            margin-right: 2px;
-
-        }
-
-        .login {
-            margin-right: -2px;
-            margin-top: 12px;
-            display: none;
-        }
-
-        .searchbox {
-            width: 60%;
-        }
-
-        .lists {
-            display: inline-block;
-        }
-
-        .moblists {
-            display: none;
-        }
-
-        .logins {
-            text-align: center;
-            margin-right: -30%;
-            margin-left: 35%;
-        }
-
         body {
             margin: 0;
-            font-size: .9rem;
-            font-weight: 400;
-            line-height: 1.6;
-            color: #212529;
-            text-align: left;
-            /* background-color: #f5f8fa; */
+            padding: 0;
+            min-height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: 'Roboto', sans-serif;
+            position: relative;
+            background: #f5f8fa;
         }
 
-        .my-form,
-        .login-form {
-            font-family: Raleway, sans-serif;
+        #vanta-background {
+            position: fixed;
+            z-index: -1;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
         }
 
-        .my-form {
-            padding-top: 1.5rem;
-            padding-bottom: 1.5rem;
-        }
-
-        .my-form .row {
-            margin-left: 0;
-            margin-right: 0;
-        }
-
-        .login-form {
-            padding-top: 1.5rem;
-            padding-bottom: 1.5rem;
-        }
-
-        .login-form .row {
-            margin-left: 0;
-            margin-right: 0;
-        }
-
-        @media only screen and (min-device-width:320px) and (max-device-width:480px) {
-            /* .mycarousel {
-            display: none;
-        }
-
-        .firstimage {
-            height: auto;
+        .login-container {
+            max-width: 400px;
             width: 90%;
+            margin: 2rem;
+            position: relative;
+            z-index: 1;
         }
 
-        .card {
-            width: 80%;
-            margin-left: 10%;
-            margin-right: 10%;
-
+        .login-card {
+            background: rgba(255, 255, 255, 0.95);
+            border-radius: 20px;
+            box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
         }
 
-        .col {
-            margin-top: 20px;
-        } */
+        .login-header {
+            background: #2c3e50;
+            padding: 2rem 1.5rem;
+            text-align: center;
+            color: white;
+        }
 
-            .right {
-                display: none;
-                background-color: #ff5500;
+        .login-header i {
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+            color: #3498db;
+        }
+
+        .login-body {
+            padding: 2rem;
+        }
+
+        .form-group {
+            margin-bottom: 1.5rem;
+        }
+
+        .form-control {
+            border: 2px solid #e1e8ef;
+            border-radius: 50px;
+            padding: 0.8rem 1.2rem;
+            transition: all 0.3s;
+        }
+
+        .form-control:focus {
+            border-color: #3498db;
+            box-shadow: 0 0 0 3px rgba(52, 152, 219, 0.1);
+            outline: none;
+        }
+
+        .btn-login {
+            background: #3498db;
+            color: white;
+            border: none;
+            border-radius: 50px;
+            padding: 0.8rem 2rem;
+            font-weight: 500;
+            width: 100%;
+            transition: all 0.3s;
+        }
+
+        .btn-login:hover {
+            background: #2980b9;
+            transform: translateY(-2px);
+        }
+
+        .login-links {
+            text-align: center;
+            margin-top: 1.5rem;
+        }
+
+        .login-links a {
+            color: #2c3e50;
+            text-decoration: none;
+            font-weight: 500;
+            display: block;
+            margin: 0.5rem 0;
+            transition: color 0.3s;
+        }
+
+        .login-links a:hover {
+            color: #3498db;
+        }
+
+        @media (max-width: 480px) {
+            .login-container {
+                margin: 1rem;
             }
-
-            /* 
-            .settings{
-            margin-left:79%;
-        } */
-            .left {
-                display: flex;
-            }
-
-            .moblogo {
-                display: none;
-            }
-
-            .logins {
-                text-align: center;
-                margin-right: 35%;
-                padding: 15px;
-            }
-
-            .searchbox {
-                width: 95%;
-                margin-right: 5%;
-                margin-left: 0%;
-            }
-
-            .moblists {
-                display: inline-block;
-                text-align: center;
-                width: 100%;
-            }
-
-            /* .pic{
-        height:auto;
-    } */
-
-            /* .mobtext{
-        display:none;
-    }
-    .destext{
-        display:inline-block;
-        width:90%;
-        margin-left: 5%;
-        margin-right: 5%;
-    } */
         }
     </style>
 </head>
 
 <body>
-
-    <main class="my-form">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header border border-dark" style="background-color:#292b2c">
-                            <h4 style="font-style:bold;color:goldenrod;text-align:left">Login</h4>
-                        </div>
-                        <div class="card-body border border-dark">
-                            <form name="my-form" action="BuyerLogin.php" method="post">
-
-                                <div class="form-group row">
-                                    <label for="phone_number" class="col-md-4 col-form-label text-md-right  "><b><i class="fas fa-phone-alt mr-2"></i>Phone Number</b></label>
-                                    <div class="col-md-6">
-                                        <input type="text" id="phone_number" class="form-control border border-dark" name="phonenumber" placeholder="Phone Number" required>
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label for="p1" class="col-md-4 col-form-label text-md-right"><b><i class="fas fa-lock mr-2"></i>Password</b></label>
-                                    <div class="col-md-6">
-                                        <input id="p1" class="form-control border border-dark" type="password" name="password" placeholder="Password" required>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn " style="background-color:#292b2c;color:goldenrod" name="login" value="Login">
-                                        Login
-                                    </button>
-                                </div>
-                                <br>
-                                <div class="col-md-6 offset-md-4">
-                                    <label id="forgotPassword"><a id='link' href="BuyerForgotPassword.php"><b style="color:black"> Forgot your password ?</b></a></label>
-                                    <br>
-                                    <label id="account"><a id='link' href="BuyerRegistration.php"><b style="color:black">Create New Account</b></a></label>
-                                </div>
-                            </form>
-                        </div>
+    <div id="vanta-background"></div>
+    <div class="login-container">
+        <div class="login-card">
+            <div class="login-header">
+                <i class="fas fa-user-circle"></i>
+                <h4>Buyer Login</h4>
+            </div>
+            <div class="login-body">
+                <form action="BuyerLogin.php" method="post">
+                    <div class="form-group">
+                        <label><i class="fas fa-phone-alt mr-2"></i>Phone Number</label>
+                        <input type="text" class="form-control" name="phonenumber" placeholder="Enter your phone number" required>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label><i class="fas fa-lock mr-2"></i>Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Enter your password" required>
+                    </div>
+                    <button type="submit" class="btn btn-login" name="login">
+                        <i class="fas fa-sign-in-alt mr-2"></i>Login
+                    </button>
+                    <div class="login-links">
+                        <a href="BuyerForgotPassword.php"><i class="fas fa-key mr-2"></i>Forgot Password?</a>
+                        <a href="BuyerRegistration.php"><i class="fas fa-user-plus mr-2"></i>Create New Account</a>
+                    </div>
+                </form>
             </div>
         </div>
-    </main>
+    </div>
 
+    <script>
+        VANTA.WAVES({
+            el: "#vanta-background",
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            color: 0x3498db,
+            shininess: 30.00,
+            waveHeight: 20.00,
+            waveSpeed: 0.50,
+            zoom: 0.75
+        });
+    </script>
 </body>
-
 </html>
 
 <?php
